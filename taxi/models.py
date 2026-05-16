74be7e7b02226f81b1bea1e84d1d29629f1c541f
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-05-16 10:47:44 +0300

    Implement all views and templates

--- taxi/models.py
+++ taxi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 
@@ -8,7 +9,10 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(max_length=20, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
 
 
 class Car(models.Model):
@@ -17,3 +21,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def get_absolute_url(self):
+        return reverse("taxi:car-detail", kwargs={"pk": self.pk})
